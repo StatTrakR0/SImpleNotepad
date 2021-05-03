54938dd54b5e3c0a95702690f323c16d3226eb76
StatTrak <wireless.00.d@gmail.com>
2021-05-03 18:18:10 +0300

    Добавьте файлы проекта.

---
+++ SImpleNotepad/ViewModels/MainViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SImpleNotepad.Models;
+
+namespace SImpleNotepad.ViewModels
+{
+    public class MainViewModel
+    {
+        private DocumentModel _document;
+        public EditorViewMoel Editor { get; set; }
+        public FileViewModel File { get; set; }
+        public HelpViewModel Help { get; set; }
+
+        public MainViewModel()
+        {
+            _document = new DocumentModel();
+            Help = new HelpViewModel();
+            Editor = new EditorViewMoel(_document);
+            File = new FileViewModel(_document);
+
+        }
+    }
+}
